663860b99b392f3a28feef7d3a41dda866e2a714
stewie-gil <gilliganstewie7@gmaail.co>
2023-05-29 08:42:39 -0700

    added readme

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This is our first views file to serve status and stats route
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
@@ -11,16 +15,19 @@ classes = {"amenities": "Amenity",
            "states": "State",
            "users": "User"}
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """Serves the route /status """
     status = {"status": "OK"}
     return jsonify(status)
 
+
 @app_views.route('/stats', methods=['GET'])
 def get_count():
+    """Returns count of items in storage"""
     a = {}
 
     for k, v in classes.items():
         a[k] = storage.count(v)
-        
     return jsonify(a)
