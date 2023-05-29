efcd35f2dbe3232e92ed318daffebe29c65cdb0b
stewie-gil <gilliganstewie7@gmaail.co>
2023-05-29 08:20:07 -0700

    fixed pycodestye

---
+++ models/engine/README.md
@@ -0,0 +1,1 @@
+This is the storage engine of our AirBnB project. 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-11;rgb:0000/0000/0000"""
+"""
 Contains the FileStorage class
 """
 
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
