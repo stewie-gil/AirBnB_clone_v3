40acb702ab0b4969da5342d42d918467e0963e19
stewie-gil <gilliganstewie7@gmaail.co>
2023-05-29 09:23:44 -0700

    added error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Our api application module"""
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """teardown method for storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """ Handles requests errors not found"""
+    return jsonify({"error":"Not found"}), 404
 
 
 if __name__ == "__main__":
